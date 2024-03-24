ee45b033859f3c465d9d5bc0d7ca67477b9a202a
Shmiddty <corey.wade.hart@gmail.com>
2024-03-23 18:41:10 -0600

    Added filter for save data. cleaned up the display

--- src/components/SaveData.vue
+++ src/components/SaveData.vue
@@ -1,34 +1,37 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const saveData = defineModel()
 
 function updater(key, e) {
   saveData.value.h[key] = e
 }
-const keys = computed(() => {
-  var out = Object.keys(saveData.value?.h ?? {}).sort((a, b) =>
-    a.toLowerCase() < b.toLowerCase() ? -1 : 1
-  )
-  return out
-})
 
 function getVal(key) {
   return saveData.value.h[key]
 }
+
+const query = ref('')
+
+const keys = computed(() => {
+  const out = Object.keys(saveData.value?.h ?? {})
+    .sort((a, b) => (a.toLowerCase() < b.toLowerCase() ? -1 : 1))
+    .filter((v) => v.toLowerCase().includes(query.value.toLowerCase()))
+  return out
+})
 </script>
 
 <template>
-  <v-card title="Save Data" style="margin-top: 1em">
+  <v-card title="Save Data" style="margin-top: 0.5em">
     <v-card-text>
+      <v-text-field label="Filter" variant="underlined" v-model="query" />
       <v-sheet
         class="overflow-auto d-flex flex-wrap justify-space-between"
-        height="300"
+        style="max-height: 300px"
       >
         <template v-for="key in keys" :key="key">
           <v-text-field
-            class="order-0"
-            style="width: 100%"
+            class="order-0 data-field-wide"
             v-if="typeof getVal(key) === 'number'"
             :label="key"
             type="number"
@@ -36,7 +39,7 @@ function getVal(key) {
             @update:model-value="updater(key, +$event)"
           />
           <v-checkbox
-            class="d-inline-flex order-1"
+            class="d-inline-flex order-1 data-field"
             v-else
             :label="key"
             :model-value="getVal(key)"
@@ -48,4 +51,14 @@ function getVal(key) {
   </v-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.data-field-wide {
+  display: inline-block;
+  width: 100%;
+}
+.data-field {
+  display: inline-block;
+  width: 30%;
+  margin: 0 0.5em;
+}
+</style>
